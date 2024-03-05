541634d5ee769d4e27407c5a16181d98be8cc0e6
kkvaldys <145556383+kkvaldys@users.noreply.github.com>
2024-03-05 00:29:28 +0000

    Deploying to gh-pages from @ kkvaldys/goit-js-hw-09@e61ac7eeec1c7431ef49ae6069f014bfa76ad2e3 🚀

--- commonHelpers2.js
+++ commonHelpers2.js
@@ -1,2 +1,2 @@
-import"./assets/modulepreload-polyfill-3cfb730f.js";const n=document.querySelector(".feedback-form");function o(e){const t=e.email.value,a=e.message.value;return{mail:t,message:a}}n.addEventListener("input",e=>{e.preventDefault();const t=o(e.currentTarget),a=JSON.stringify(t);localStorage.setItem("message",a)});
+import"./assets/modulepreload-polyfill-3cfb730f.js";const n=document.querySelector(".feedback-form");function o(e){const t=e.email.value,a=e.message.value;return{email:t,message:a}}n.addEventListener("input",e=>{e.preventDefault();const t=o(e.currentTarget),a=JSON.stringify(t);localStorage.setItem("message",a)});
 //# sourceMappingURL=commonHelpers2.js.map
